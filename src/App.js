1a65609752cc2ca1004b68e934eaafdbea4395aa
Aneesh054 <aneeshaneesh054@gmail.com>
2021-06-01 11:33:40 +0530

    Carousel

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Alert, Button } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <h1 className="bg-primary text-light p-2 d-flex justify-content-center">React Bootstrap</h1>
+      <h1 className="bg-dark text-light p-2">Carousel demo</h1>
 
-      
-      <Button variant="success">React Bootstrap Button</Button>
-
-    
-      <Alert variant="info">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </Alert>
+      <Carousel>
+        {[1, 2, 3].map((item, index) => (
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={"https://picsum.photos/100" + index}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 }
